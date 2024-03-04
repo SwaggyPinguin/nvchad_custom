d35ff71297b23482fc5c3d23d214841c636d4342
Noah Dahms <noahdahms@gmail.com>
2024-03-04 08:43:44 +0100

    added keybinding for toggeling trancparency

--- mappings.lua
+++ mappings.lua
@@ -2,14 +2,20 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-m>"] = { ":Mason <CR>", "open Mason"},
-    ["<leader>l"] = { ":Lazy <CR>", "open Lazy"}
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<C-m>"] = { ":Mason <CR>", "open Mason" },
+		["<leader>l"] = { ":Lazy <CR>", "open Lazy" },
+		["<leader>tt"] = {
+			function()
+				require("base46").toggle_transparency()
+			end,
+			"Toggle transparency",
+		},
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
 }
 
 -- more keybinds!
