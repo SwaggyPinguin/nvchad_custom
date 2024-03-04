c39a8acd0e73a4aed0ac61b8873b5ec49ddc606b
Noah Dahms <noahdahms@gmail.com>
2024-03-04 08:44:23 +0100

    gruvbox setup file

--- configs/gruvbox.lua
+++ configs/gruvbox.lua
@@ -1,5 +1,5 @@
 -- Default options:
-require("gruvbox").setup({
+require("gruvbox").setup {
 	enabled = true,
 	terminal_colors = true, -- add neovim terminal colors
 	undercurl = true,
@@ -23,5 +23,6 @@ require("gruvbox").setup({
 	overrides = {},
 	dim_inactive = false,
 	transparent_mode = true,
-})
-vim.cmd "colorscheme gruvbox"
+}
+-- vim.cmd "colorscheme gruvbox"
+vim.cmd([[colorscheme gruvbox]])
